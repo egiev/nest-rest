de5596f8a0fa85859d9d44ba58bd7b0ed667c388
Reginald Mabanta <reginaldventura23@gmail.com>
2025-01-23 14:34:46 +0800

    setup database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReportModule } from './report/report.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: (process.env.DATABASE_TYPE as any) || 'DATABASE',
+      host: process.env.DATABASE_HOST,
+      database: process.env.DATABASE_NAME,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      port: parseInt(process.env.DATABASE_PORT!, 10),
+      synchronize: true,
+      entities: [],
+    }),
+    UserModule,
+    ReportModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
