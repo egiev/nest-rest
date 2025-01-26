5797b738d62d933219ca7986333229703a7a3ab0
Reginald Mabanta <reginaldventura23@gmail.com>
2025-01-26 11:44:06 +0800

    implement database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,14 @@
-import { Module, OnModuleInit } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './infrastructure/database/database.module';
 import { ReportModule } from './report/report.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: (process.env.DATABASE_TYPE as any) || 'DATABASE',
-      host: process.env.DATABASE_HOST,
-      database: process.env.DATABASE_NAME,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      port: parseInt(process.env.DATABASE_PORT!, 10),
-      synchronize: true,
-      entities: [],
-    }),
-    UserModule,
-    ReportModule,
-  ],
+  imports: [ConfigModule.forRoot(), DatabaseModule, UserModule, ReportModule],
   controllers: [AppController],
   providers: [AppService],
 })
